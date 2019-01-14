eb0ef42bfa325ba400ceb5c0486f7ac6b0671d32
Javi <javikiller5000@gmail.com>
2019-01-14 16:34:39 +0100

    que pacha

--- prueba de gitkraken/prueba de gitkraken/Program.cs
+++ prueba de gitkraken/prueba de gitkraken/Program.cs
@@ -10,6 +10,7 @@ namespace prueba_de_gitkraken
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("me llamo willy manfederic jhonson");
+        }   
     }
 }

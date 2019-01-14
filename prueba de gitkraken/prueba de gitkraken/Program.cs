ee41b6548ba38f3ca9d895d11ca19b862adcb24c
Javi <javikiller5000@gmail.com>
2019-01-14 16:15:57 +0100

    esto es lo primerito que hago
    y no se que es jejeje

---
+++ prueba de gitkraken/prueba de gitkraken/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba_de_gitkraken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
